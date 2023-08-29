22d9721a8155f5d9a6f0d4c8d01da59687e5b139
Eduard Khiaev <eddietheone1984@gmail.com>
2023-08-29 19:51:00 +0000

    hostname

--- npm/index.js
+++ npm/index.js
@@ -1,8 +1,24 @@
 const express = require('express');
+const os = require('os');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/', (req, res) => {
+  const hostname = os.hostname();
+  const timestamp = new Date().toISOString();
+  const networkInterfaces = os.networkInterfaces();
+  const ips = Object.values(networkInterfaces)
+    .flat()
+    .map((iface) => iface.address);
+
+  res.json({
+    'Data': {
+      'Hostname': hostname,
+      'Timestamp': timestamp,
+      'IP Addresses': ips
+    },
+  });
+
+});
 
 app.listen(port, () => console.log(`App running on port ${port}`));
-
